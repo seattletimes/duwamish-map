c3f3a8a3b6ce957040da58ebe9a0355f324d57f9
Thomas Wilburn <news@thomaswilburn.net>
2015-09-30 15:15:03 -0700

    Add the river line

--- src/js/main.js
+++ src/js/main.js
@@ -5,11 +5,17 @@
 require("component-responsive-frame/child");
 require("component-leaflet-map");
 
+var path = require("./river");
+
 var mapElement = document.querySelector(".duwamish");
+var L = mapElement.leaflet;
 var map = mapElement.map;
 var points = Object.keys(mapElement.lookup).sort().map(k => mapElement.lookup[k]);
 
 map.fitBounds([[47.1019, -122.412],[47.614,-121.262]]);
+
+var river = L.polyline(path, { color: "#005", opacity: 1, weight: 2, smoothFactor: 1.5 });
+river.addTo(map);
 
 var index = -1;
 
